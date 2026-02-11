33a628884c21960f727ba7db8b711e19a0cd77be
Mikołaj <kaleta222@gmail.com>
2026-02-11 13:32:43 +0100

    add --verbose flag to print more details

--- crack.py
+++ crack.py
@@ -1,7 +1,8 @@
 import click, csv
 
 @click.command()
-def hello():
+@click.option("-v", "--verbose", is_flag=True, help="Be more verbose")
+def hello(verbose):
     with open('to-crack.txt', 'r', encoding='utf-8') as f:
         to_crack_list = f.readlines()
     to_crack_list = [line.strip() for line in to_crack_list]
@@ -13,13 +14,26 @@ def hello():
             for hash in to_crack_list:
 
                 if row[1] == hash:
-                    print("Password cracked!", hash, '->', row[0])
+                    if verbose: print("Password cracked!")
+                    print(hash, '->', row[0])
                     cracked_list.append((hash, row[0]))
                     to_crack_list.remove(hash)
             if len(cracked_list) == len(to_crack_list):
                 break # stop the loop if all passwords have been cracked
             
         print(cracked_list)
+
+        if verbose:
+            if to_crack_list:
+                n_cracked = len(cracked_list)
+                n_not_cracked = len(to_crack_list)
+                print("Cracked:",n_cracked, f"({n_cracked/(n_cracked+n_not_cracked)*100}%)")
+                print("Not Cracked:",n_not_cracked, f"({n_not_cracked/(n_cracked+n_not_cracked)*100}%)")
+                print("List of cracked passwords:", cracked_list)
+                print("List of not cracked passwords:", to_crack_list)
+            else: 
+                print("All passwords have been cracked!")
+                print("List of cracked passwords:", cracked_list)
 
 
 if __name__ == '__main__':
