47f35446b7997e15bdda15f6928ceb27136f9858
Mikołaj <kaleta222@gmail.com>
2026-02-12 10:17:23 +0100

    improve verbosity

--- crack.py
+++ crack.py
@@ -1,4 +1,4 @@
-import click, csv
+import click, csv, time
 
 def predict_hash_algorithm(hash_str: str) -> int | None:
     """
@@ -46,7 +46,9 @@ def return_algorithm_name(i: int) -> str:
     default='to-crack.txt',
     help="Path to .txt file with passwords to crack."
 )
-def hello(verbose, algorithm, hash_file):
+def hello(verbose, algorithm, hash_file, output):
+
+    start_time = time.time()
     with open(hash_file, 'r', encoding='utf-8') as f:
         hashed_passwords = f.readlines()
     to_crack_list = [line.strip() for line in hashed_passwords]
@@ -68,7 +70,7 @@ def hello(verbose, algorithm, hash_file):
             hash_algo = None
 
     if verbose and not algorithm:
-        print("Detected hashing algorithm:", return_algorithm_name(hash_algo))
+        print("Detected hashing algorithm:", return_algorithm_name(hash_algo), '\n')
 
     with open('hashes.csv', 'r', encoding='utf-8') as csv_file:
         csv_reader = csv.reader(csv_file)
@@ -80,26 +82,39 @@ def hello(verbose, algorithm, hash_file):
                     if not hash_algo: continue
 
                 if row[hash_algo] == hash:
-                    if verbose: print("Password cracked!")
-                    print(hash, '->', row[0])
+                    if verbose: 
+                        click.secho("Password cracked!", bg='green')
+                        print(hash, '->', row[0])
                     cracked_list.append((hash, row[0]))
                     to_crack_list.remove(hash)
             if len(cracked_list) == len(hashed_passwords):
                 break # stop the loop if all passwords have been cracked
             
-        print(cracked_list)
 
         if verbose:
-            if to_crack_list:
-                n_cracked = len(cracked_list)
-                n_not_cracked = len(to_crack_list)
-                print("Cracked:",n_cracked, f"({n_cracked/(n_cracked+n_not_cracked)*100}%)")
-                print("Not Cracked:",n_not_cracked, f"({n_not_cracked/(n_cracked+n_not_cracked)*100}%)")
-                print("List of cracked passwords:", cracked_list)
+            n_cracked = len(cracked_list)
+            n_not_cracked = len(to_crack_list)
+            fg_color = 'green' if not to_crack_list else ('red' if n_cracked == 0 else 'yellow')
+            click.secho("\n======================", fg=fg_color)
+            print("Cracked:",n_cracked, f"({n_cracked/(n_cracked+n_not_cracked)*100}%)")
+            print("Not Cracked:",n_not_cracked, f"({n_not_cracked/(n_cracked+n_not_cracked)*100}%)")
+            click.secho("======================\n", fg=fg_color)
+            if n_not_cracked:
                 print("List of not cracked passwords:", to_crack_list)
-            else: 
-                print("All passwords have been cracked!")
-                print("List of cracked passwords:", cracked_list)
+            if not to_crack_list: 
+                click.secho("All passwords have been cracked! Check the output.csv file.", bg='green')
+        else:
+            for item in cracked_list:
+                print(item[0], '->', item[1]) 
+            if not cracked_list:
+                click.secho("Couldn't crack any password", fg='red')
+
+
+        end_time = time.time()
+        if verbose:
+            elapsed_time = end_time - start_time
+            if elapsed_time > 1: elapsed_time = int(elapsed_time)
+            print(f"\nTime elapsed: {elapsed_time} seconds")
 
 
 if __name__ == '__main__':
